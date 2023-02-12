45a47ddc39714eaf6e3a7286f30a367290bdbc46
Viphava280444 <87804447+Viphava280444@users.noreply.github.com>
2023-02-12 22:50:34 +0700

    Manage File and Folder

---
+++ GUI_Chat/tkinter_intro.py
@@ -0,0 +1,23 @@
+import tkinter
+
+#Define window
+root = tkinter.Tk()
+root.title("Let's Chat")
+root.iconbitmap("talk_icon.ico")
+root.geometry("400x600")
+root.resizable(0,0)
+
+#Define colors
+root_color = "#535657"
+input_color = "#4f646f"
+output_color = "#dee7e7"
+root.config(bg=root_color)
+
+#Define function
+
+
+#Define GUI Layout
+
+
+#Run the root window's mainloop()
+root.mainloop()
